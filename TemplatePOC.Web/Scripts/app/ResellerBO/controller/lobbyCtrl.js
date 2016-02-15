cd12e5c56c25d7233c433ca93b678293a0b2597f
arthur.chang <arthur.chang@afusion.com>
2016-02-15 18:57:50 +0800

    將CSS-Editor封裝成Directive: cssEditor

--- TemplatePOC.Web/Scripts/app/ResellerBO/controller/lobbyCtrl.js
+++ TemplatePOC.Web/Scripts/app/ResellerBO/controller/lobbyCtrl.js
@@ -9,19 +9,6 @@
     function lobbyCtrl(lobbySvc, gameGroupBoxSvc, $log, $state, $stateParams) {
         var vm = this;
         vm.IsAdd = !$stateParams.id;
-        vm.editorOptions = {
-            lineNumbers: true,
-            gutters: ["CodeMirror-lint-markers"],
-            height: 128,
-            lint:true,
-            indentUnit:4,
-            showHint:true,
-            inputStyle: 'textarea',
-            theme: 'night',
-            extraKeys: {"Ctrl-Z":"autocomplete"},
-            keyMap:'sublime',
-            mode:'css'
-        };
         vm.model = {
             Name: '',
             Description: '',
@@ -35,34 +22,8 @@
         vm.AddGameGroup = gameGroupBoxSvc.AddGameGroup;
         vm.UpdateGameGroup = gameGroupBoxSvc.UpdateGameGroup;
         vm.RemoveGameGroup = gameGroupBoxSvc.RemoveGameGroup;
-        vm.LoadCodeMirror = loadCodeMirror;
         vm.OnPreview = onPreview;
         vm.OnSave = onSave;
-
-        function loadCodeMirror(target) {
-            var timeouts = [];
-            var singleQuoteFlag = false,
-                    quoteFlag = false;
-
-            target.on("change", function (editor, change) {
-                
-                if (change.origin === '+input') {
-                    var text = change.text.toString();
-
-                    if (text.match(/'/))
-                        singleQuoteFlag = !singleQuoteFlag;
-                    if (text.match(/"/))
-                        quoteFlag = !quoteFlag;
-
-                    if (!text.match(/[\s()\[\]{};:@"']/) && !singleQuoteFlag && !quoteFlag)
-                        timeouts.push(setTimeout(function() {
-                            editor.execCommand("autocomplete");
-                        }, 250));             
-                    else
-                        timeouts.splice(0, timeouts.length);
-                }
-            });
-        }
 
         function onPreview() { }
 
---
+++ TemplatePOC.Web/Scripts/app/_Common/directive/cssEditor/cssEditor.js
@@ -0,0 +1,63 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app.common")
+        .directive("cssEditor", cssEditor);
+
+    function cssEditor() {
+        return {
+            restrict: "EA",
+            replace: true,
+            scope: {
+                content: "="
+            },
+            templateUrl: "/Scripts/app/_Common/directive/cssEditor/cssEditor.html",
+            controller: ["$scope",fnCtrl]
+        };
+
+        function fnCtrl($scope) {
+            var vm = $scope;
+            vm.editorOptions = {
+                lineNumbers: true,
+                gutters: ["CodeMirror-lint-markers"],
+                height: 128,
+                lint: true,
+                indentUnit: 4,
+                showHint: true,
+                inputStyle: 'textarea',
+                theme: 'night',
+                extraKeys: { "Ctrl-Z": "autocomplete" },
+                keyMap: 'sublime',
+                mode: 'css'
+            };
+
+            vm.LoadCodeMirror = loadCodeMirror;
+
+            function loadCodeMirror(target) {
+                var timeouts = [];
+                var singleQuoteFlag = false,
+                    quoteFlag = false;
+
+                target.on("change", function (editor, change) {
+
+                    if (change.origin === '+input') {
+                        var text = change.text.toString();
+
+                        if (text.match(/'/))
+                            singleQuoteFlag = !singleQuoteFlag;
+                        if (text.match(/"/))
+                            quoteFlag = !quoteFlag;
+
+                        if (!text.match(/[\s()\[\]{};:@"']/) && !singleQuoteFlag && !quoteFlag)
+                            timeouts.push(setTimeout(function () {
+                                editor.execCommand("autocomplete");
+                            }, 250));
+                        else
+                            timeouts.splice(0, timeouts.length);
+                    }
+                });
+            }
+        }
+
+    }
+})();
